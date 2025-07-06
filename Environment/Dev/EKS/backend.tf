f076a471c4bfcdf6dbba06a86724048ed32b2ffe
mayur702 <mayurmagar702@gmail.com>
2025-07-06 22:33:24 +0530

    this is vpc and eks create code

--- Environment/Dev/EKS/backend.tf
+++ Environment/Dev/EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mayur-terraform-states"
-    key    = "Dev/EKS/terraformeks.tfstate"
+    key    = "Dev/EKS/terraformekst.tfstate"
     region = "us-east-1"
   }
 }
